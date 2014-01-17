96570884bd5b9fe0c66eb69e1cd222deff505f79
mika <mika.simanainen@gmail.com>
2014-01-17 19:41:14 +0200

    add licence

--- i18next.php
+++ i18next.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <github.com/Mika-> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return
+ * ----------------------------------------------------------------------------
+ */
+
 class i18next {
 
 	private $_path = null;
